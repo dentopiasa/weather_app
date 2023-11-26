4cebb64d099be0ed8c35ce9308bb85691cc114ba
dentopiasa <castelli_philippe@hotmail.com>
2023-11-26 17:39:15 +0200

    completed core function

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const citySearch = document.querySelector("#cityInput").value;
 const displayCity = document.querySelector("#displayCity");
 const displayTemp = document.querySelector("#displayTemp");
 const displayTime = document.querySelector("#displayTime");
-
-
-const apiKey = "f0d24420e44943d199d160311232311";
-const apiUrl = "http://api.weatherapi.com/v1/current.json&q=London";
-
-
+const submitBtn = document.querySelector("#submitBtn");
 
 async function checkWeather() {
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=f0d24420e44943d199d160311232311&q=London');
+    const citySearch = document.querySelector("#cityInput").value;
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=f0d24420e44943d199d160311232311&q=' + `${citySearch}`);
     var data = await response.json();
     console.log(data);
     const city = data.location.name;
@@ -23,6 +18,12 @@ async function checkWeather() {
     displayTemp.textContent = temperature;
     displayTime.textContent = time;
 }
-checkWeather();
+
+submitBtn.addEventListener("click", ()=>{
+    checkWeather();
+})
+
+
+
 
  
